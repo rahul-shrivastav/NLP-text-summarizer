32196241c2337e255fe1a9165dbef25a39d30ab7
rahul shrivastava <rahulshrivastav052@gmail.com>
2025-03-20 12:45:27 +0530

    chat-bubble-added

--- frontend/src/components/Conversations.jsx
+++ frontend/src/components/Conversations.jsx
@@ -31,13 +31,13 @@ const Conversations = ({ allchat }) => {
           allchat.map((chat, index) => {
             if (index != allchat.length - 1) {
               return (
-                <div key={index} className='p-3 m-2 text-black w-fit max-w-[70%] rounded-md bg-slate-200'>
+                <div key={index} className='p-3 m-2 text-black w-fit max-w-[70%] rounded-3xl rounded-tl-none bg-slate-200'>
                   {chat}
                 </div>
               )
             } else {
               return (
-                <div key={index} className='p-3 relative mt-10 ml-3 text-black w-fit max-w-[70%] rounded-md bg-slate-200'>
+                <div key={index} className='p-3 relative  mt-10 ml-3 text-black w-fit max-w-[70%] rounded-3xl rounded-tl-none bg-slate-200'>
                   <Typewriter text={chat} speed={1} />
                   <img className='absolute -top-7 animate-bounce' src='/bot.png' width={25} />
                   <button className='cursor-pointer absolute bottom-0 -right-5  text-right flex items-center justify-end'><MdOutlineContentCopy /></button>
