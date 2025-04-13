fa4be8d16c482c523cad0131e9a72938731d52b5
rahul shrivastava <rahulshrivastav052@gmail.com>
2025-04-14 01:12:24 +0530

    frontend-major

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,22 +8,20 @@ import Inputpanel from '@/components/Inputpanel';
 
 
 export default function Home() {
-  const [filename, setfilename] = useState(null);
-  const [qid, setqid] = useState(null);
-  const [extractedtext, setextractedtext] = useState(null);
-  const [allchat, setallchat] = useState(['Hello there! How can I help you today?']);
 
+  const [mode, setmode] = useState('text')
+  console.log(mode)
   return (
     <>
-      <div className=" w-vh h-svh flex flex-col bg-gray-200     ">
-        <div className='border h-[100px] text-white flex items-center justify-center'>
-          <p className="text-5xl font-semibold bg-gradient-to-r from-blue-400 to-blue-800 bg-clip-text text-transparent">
+      <div className=" w-vh h-svh flex flex-col  bg-gray-50 bg-gradient-to-br from-gray-200 via-white to-gray-300    ">
+        <div className=' h-[100px] text-white flex flex-col borderr border-black items-center justify-end'>
+          <p className="text-5xl font-semibold bg-gradient-to-l from-blue-400 to-blue-800 bg-clip-text text-transparent">
             AI Text Summarizer
           </p>
         </div>
 
-        <div className=' border  flex-1 flex items-center justify-center'>
-          <Inputpanel />
+        <div className='   flex-1 flex items-center justify-center'>
+          <Inputpanel mode={mode} setmode={setmode} />
         </div>
 
 
--- frontend/src/components/Inputpanel.jsx
+++ frontend/src/components/Inputpanel.jsx
@@ -1,27 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaCopy } from "react-icons/fa";
+// import logo from '/logo.png';
 
-const Inputpanel = () => {
+const Inputpanel = ({ mode, setmode }) => {
+    const [loading, setloading] = useState(false);
+    const [boxOpen, setboxopen] = useState(false)
+    const [sumarizedText, setSumarizedText] = useState(null)
+
   return (
-    <div className='w-4/5 h-4/5  flex text-black bg-white '>
-        <div className="w-1/2 h-full   flex flex-col relative">
-            <div className=" m-5  ">
-                <button className="hover:cursor-pointer  px-5 py-1 bg-blue-50 rounded-sm text-blue-400 font-semibold">Text</button>
-                <button className="hover:cursor-pointer px-5 py-1 font-semibold bg-blue-800 rounded-sm text-white">PDF</button>
-            </div>
-            <div className=" flex-1  ">
-                <span className=" absolute top-1/2 text-gray-400 w-full text-center">Paste your text here...</span>
-                <textarea className='w-full h-[90%]  pl-5 focus:outline-none  focus:border-none focus:ring-0'   name="" id=""></textarea>
-            </div>
-            <div className="hover:scale-105 transition-transform  bg-gradient-to-r from-blue-400 right-10 absolute  to-blue-800 p-1 rounded-md bottom-5 w-fit h-fit">
-                <button className=' hover:cursor-pointer hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 hover:text-white  text-blue-500 font-bold  px-3 py-1 rounded-sm bg-white '>Summarize</button>
-            </div>
-        </div>
-        <div className="w-1/2 h-full border border-black ">
-        </div>
+      <div className={'w-[85%] h-[90%]  flex text-black bg-w gap-4 transition-all'}>
+          <div className="w-1/2 duration-1000 h-full shadow-2xl rounded-md  flex flex-col relative bg-white border border-slate-300">
+              <div className=" mt-5 ml-5  ">
+                  <button onClick={() => setmode('text')} className={mode === 'pdf' ? 'bg-blue-50  text-blue-400 hover:cursor-pointer px-5 py-1 font-semibold rounded-sm ' : 'bg-blue-800  text-white hover:cursor-pointer px-5 py-1 font-semibold rounded-sm '}>Text</button>
+                  <button onClick={() => setmode('pdf')} className={mode === 'text' ? 'bg-blue-50  text-blue-400 hover:cursor-pointer px-5 py-1 font-semibold rounded-sm ' : 'bg-blue-800  text-white hover:cursor-pointer px-5 py-1 font-semibold rounded-sm '}>PDF</button>
+              </div>
+              {
+                  (mode === 'text') && <div className=" flex-1  p-3">
+                      <span className=" absolute top-1/2 text-gray-400 w-full text-center text-sm">Paste your text here...</span>
+                      <textarea autoFocus className='text-sm pt-3 border border-dashed border-slate-400 w-full h-[90%]  pl-5 focus:outline-none  focus:border-nonee focus:ring-0' name="" id=""></textarea>
+                  </div>
+              }
+              {
+                  (mode === 'pdf') && <div className=" flex-1  p-3">
+                      <button className="hover:cursor-pointer absolute top-1/2 text-gray-400 w-full text-center text-sm">Click to upload PDF...</button>
+                      <button autoFocus className='text-sm bg-gray-50 pt-3 border border-dashed border-slate-400 w-full h-[90%]  pl-5 focus:outline-none  focus:border-nonee focus:ring-0 hover:cursor-pointer' name="" id=""></button>
+                  </div>
+
+              }
+
+
+              <div className="hover:scale-105 transition-all  bg-gradient-to-r from-blue-400 right-7 absolute  to-blue-800 p-1 rounded-md bottom-3 w-fit h-fit">
+                  <button onClick={() => setloading(!loading)} className=' hover:cursor-pointer hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 hover:text-white  text-blue-500 font-bold  px-3 py-1 rounded-sm bg-white '>Summarize</button>
+              </div>
+          </div>
+          <div className={"w-1/2 transition-all overflow-clip duration-500 border relative pb-10 border-slate-300 h-full shadow-2xl rounded-md bg-white p-5 flex flex-col"}>
+              <div className="text-md text-blue-500 font-bold  text-center p-5  flex items-center justify-center ">Summarized text  </div>
+              <div className="text-sm font-extralight overflow-y-scroll  text-black h-[90%]  text-center p-3   border-dotted border border-slate-500">
+                  {sumarizedText}
+              </div>
+              <button className=" text-xl text-gray-700 absolute bottom-3 right-3 hover:cursor-pointer">
+                  <FaCopy />
+
+              </button>
+
+              {loading &&
+                  <div className="w-full h-full top-0 left-0 absolute flex items-center justify-center flex-col gap-4 bg-white">
+                      <div className="border-t-2 border-blue-500 animate-spin w-10 h-10 rounded-full text-sm "></div>
+                      <div className="text-sm text-slate-400">Sumarizing...Please wait</div>
+                  </div>
+              }
+              {!sumarizedText && !loading &&
+                  <div className=" w-full h-full top-0 left-0 absolute flex items-center justify-center flex-col gap-4 bg-white">
+                      <img className='animate-pulse' src='/logo.png' width={80} alt="" />
+                  </div>
+
+              }
+          </div>
 
 
         
-    </div>
+      </div>
   )
 }
 
